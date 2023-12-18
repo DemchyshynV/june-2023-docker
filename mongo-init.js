89ead05997f9f5a8fc01d732aeb52c46740c1839
Vitaliy Dechyshyn <demchyshyn.v87@gmail.com>
2023-12-18 21:30:37 +0200

    lesson1

---
+++ mongo-init.js
@@ -0,0 +1,12 @@
+db.createUser(
+    {
+        user:'userm',
+        pwd:'userm',
+        roles:[
+            {
+                role:'readWrite',
+                db:'june-2023'
+            }
+        ]
+    }
+)
